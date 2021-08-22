e1734f59965b2b334191b1cc61052348a25dbb9d
judiy <judiy@DESKTOP-PUQESQ6>
2021-08-22 21:22:38 +0900

    202010822 21:22 커밋 앤 푸시

--- src/com/javaland/stg/panel/DungeonPanel.java
+++ src/com/javaland/stg/panel/DungeonPanel.java
@@ -10,10 +10,12 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 import javax.swing.AbstractButton;
+import javax.swing.BorderFactory;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /* 이준한 담당 패널 */
@@ -25,10 +27,16 @@ public class DungeonPanel extends JPanel {
 	private ImageIcon stageLogo1 = new ImageIcon("image/stage1.png");
 	private ImageIcon stageLogo2 = new ImageIcon("image/stage2.png");
 	private ImageIcon stageLogo3 = new ImageIcon("image/stage3.png");
-	private ImageIcon textBossLogo = new ImageIcon("image/textBoss.png");
-	private ImageIcon imageMonster1Logo = new ImageIcon("image/imageMonster1.png");
-	private ImageIcon imageMonster2Logo = new ImageIcon("image/imageMonster2.png");
-	private ImageIcon imageMonster3Logo = new ImageIcon("image/imageMonster3.png");
+
+	private ImageIcon monster1 = new ImageIcon("image/monster1.png");
+	private ImageIcon monster2 = new ImageIcon("image/monster2.png");
+	private ImageIcon monster3 = new ImageIcon("image/monster3.png");
+	private ImageIcon bossMonster1 = new ImageIcon("image/champicon.png");
+
+	private ImageIcon monster1Text = new ImageIcon("image/monster1Text.png");
+	private ImageIcon monster2Text = new ImageIcon("image/monster2Text.png");
+	private ImageIcon monster3Text = new ImageIcon("image/monster3Text.png");
+	private ImageIcon bossMonster1Text = new ImageIcon("image/bossMonster1Text.png");
 
 	private MainPanel mainPanel;
 	private ScriptPanel scriptPanel;
@@ -39,14 +47,26 @@ public class DungeonPanel extends JPanel {
 	private AdminPanel adminPanel;
 	private StorePanel storePanel;
 
-	private JButton returnLogoButton;
-	private JButton stageLogo1Button;
-	private JButton imageMonster1LogoButton;
-	private JButton imageMonster2LogoButton;
-	private JButton imageMonster3LogoButton;
-	private JButton textBossLogoButton;
+	private JButton returnLogoBtn;
+	private JButton stageLogo1Btn;
 
-	/* 이준한 담당 패널 */
+	/* 몬스터 이미지 */
+	private JButton monster1Btn;
+	private JButton monster2Btn;
+	private JButton monster3Btn;
+	private JButton bossMonster1Btn;
+
+	/* 몬스터 이름 */
+//	private JButton monster1TextBtn;
+//	private JButton monster2TextBtn;
+//	private JButton monster3TextBtn;
+//	private JButton bossMonster1TextBtn;
+	private JLabel monster1Textlb;
+	private JLabel monster2Textlb;
+	private JLabel monster3Textlb;
+	private JLabel bossMonster1Textlb;
+	
+
 	public DungeonPanel() {
 		dungeonPanel = this;
 		this.setBounds(0, 0, 1600, 900);
@@ -54,68 +74,157 @@ public class DungeonPanel extends JPanel {
 		this.setBackground(Color.BLACK);
 
 		/* 리턴로고부착 */
-		returnLogoButton = new JButton();
-		returnLogoButton.setIcon(returnLogo);
-		returnLogoButton.setBounds(70, 170, 360, 200);
-		returnLogoButton.setBorderPainted(false);
-		returnLogoButton.setContentAreaFilled(false);
-		returnLogoButton.setFocusPainted(false);
+		returnLogoBtn = new JButton();
+		returnLogoBtn.setIcon(returnLogo);
+		returnLogoBtn.setBounds(15, 150, 394, 148);
+		returnLogoBtn.setBorderPainted(false);
+		returnLogoBtn.setContentAreaFilled(false);
+		returnLogoBtn.setFocusPainted(false);
 
 		/* 스테이지 1 로고부착 */
-		stageLogo1Button = new JButton();
-		stageLogo1Button.setIcon(stageLogo1);
-		stageLogo1Button.setBounds(572, 164, 456, 134);
-		stageLogo1Button.setBorderPainted(false);
-		stageLogo1Button.setContentAreaFilled(false);
-		stageLogo1Button.setFocusPainted(false);
+		stageLogo1Btn = new JButton();
+		stageLogo1Btn.setIcon(stageLogo1);
+		stageLogo1Btn.setBounds(572, 164, 456, 134);
+		stageLogo1Btn.setBorderPainted(false);
+		stageLogo1Btn.setContentAreaFilled(false);
+		stageLogo1Btn.setFocusPainted(false);
 
-		/* 던전 몬스터 1 버튼부착 */
-		imageMonster1LogoButton = new JButton();
-		imageMonster1LogoButton.setIcon(imageMonster1Logo);
-		imageMonster1LogoButton.setBounds(148, 408, 129, 135);
-		imageMonster1LogoButton.setBorderPainted(false);
-		imageMonster1LogoButton.setContentAreaFilled(false);
-		imageMonster1LogoButton.setFocusPainted(false);
+		/* 던전 몬스터 1 이미지 버튼부착 */
+		monster1Btn = new JButton();
+		monster1Btn.setIcon(monster1);
+		monster1Btn.setBounds(148, 408, 129, 135);
+		monster1Btn.setBorderPainted(false);
+		monster1Btn.setContentAreaFilled(false);
+		monster1Btn.setFocusPainted(false);
 
-		/* 던전 몬스터 2 버튼부착 */
-		imageMonster2LogoButton = new JButton();
-		imageMonster2LogoButton.setIcon(imageMonster2Logo);
-		imageMonster2LogoButton.setBounds(534, 408, 129, 134);
-		imageMonster2LogoButton.setBorderPainted(false);
-		imageMonster2LogoButton.setContentAreaFilled(false);
-		imageMonster2LogoButton.setFocusPainted(false);
-
-		/* 던전 몬스터 3 버튼부착 */
-		imageMonster3LogoButton = new JButton();
-		imageMonster3LogoButton.setIcon(imageMonster3Logo);
-		imageMonster3LogoButton.setBounds(915, 398, 155, 153);
-		imageMonster3LogoButton.setBorderPainted(false);
-		imageMonster3LogoButton.setContentAreaFilled(false);
-		imageMonster3LogoButton.setFocusPainted(false);
-
-		/* 보스 몬스터 1 버튼부착 */
-		textBossLogoButton = new JButton();
-		textBossLogoButton.setIcon(textBossLogo);
-		textBossLogoButton.setBounds(1268, 513, 164, 103);
-		textBossLogoButton.setBorderPainted(false);
-		textBossLogoButton.setContentAreaFilled(false);
-		textBossLogoButton.setFocusPainted(false);
-
+		/* 던전 몬스터 2 이미지 버튼부착 */
+		monster2Btn = new JButton();
+		monster2Btn.setIcon(monster2);
+		monster2Btn.setBounds(514, 408, 129, 135);
+		monster2Btn.setBorderPainted(false);
+		monster2Btn.setContentAreaFilled(false);
+		monster2Btn.setFocusPainted(false);
+		
+		/* 던전 몬스터 3 이미지 버튼부착 */
+		monster3Btn = new JButton();
+		monster3Btn.setIcon(monster3);
+		monster3Btn.setBounds(915, 408, 155, 153);
+		monster3Btn.setBorderPainted(false);
+		monster3Btn.setContentAreaFilled(false);
+		monster3Btn.setFocusPainted(false);
+		
+		/* 보스 몬스터 1 이미지 버튼부착 */
+		bossMonster1Btn = new JButton();
+		bossMonster1Btn.setIcon(bossMonster1);
+		bossMonster1Btn.setBounds(1280, 340, 220, 220);
+		bossMonster1Btn.setBorderPainted(false);
+		bossMonster1Btn.setContentAreaFilled(false);
+		bossMonster1Btn.setFocusPainted(false);
+		
+		/* 던전 몬스터 1 이름 라벨 부착 */ 
+		monster1Textlb = new JLabel();
+		monster1Textlb.setIcon(monster1Text);
+		monster1Textlb.setBounds(89, 543, 247, 103);
+		
+		/* 던전 몬스터 2 이름 라벨 부착 */ 
+		monster2Textlb = new JLabel();
+		monster2Textlb.setIcon(monster2Text);
+		monster2Textlb.setBounds(455, 543, 247, 103);
+		
+		/* 던전 몬스터 3 이름 라벨 부착 */ 
+		monster3Textlb = new JLabel();
+		monster3Textlb.setIcon(monster3Text);
+		monster3Textlb.setBounds(869, 543, 247, 103);
+		
+		/* 보스 몬스터 1 이름 라벨 부착 */ 
+		bossMonster1Textlb = new JLabel();
+		bossMonster1Textlb.setIcon(bossMonster1Text);
+		bossMonster1Textlb.setBounds(1310, 543, 247, 103);
+		
 		/* 버튼 부착 */
 		dungeonPanel.add(returnLogo);
-		dungeonPanel.add(stageLogo1);
-		dungeonPanel.add(imageMonster1Logo);
-		dungeonPanel.add(imageMonster2Logo);
-		dungeonPanel.add(imageMonster3Logo);
-		dungeonPanel.add(textBossLogoButton);
-
+		dungeonPanel.add(monster1Textlb);
+		dungeonPanel.add(monster2Textlb);
+		dungeonPanel.add(monster3Textlb);
+		dungeonPanel.add(bossMonster1Textlb);
+		
+		
 		/* 버튼 구현 */
-		this.add(returnLogoButton);
-		this.add(stageLogo1Button);
-		this.add(imageMonster1LogoButton);
-		this.add(imageMonster2LogoButton);
-		this.add(imageMonster3LogoButton);
-		this.add(textBossLogoButton);
+		this.add(returnLogoBtn);
+		this.add(stageLogo1Btn);
+		this.add(monster1Btn);
+		this.add(monster2Btn);
+		this.add(monster3Btn);
+		this.add(bossMonster1Btn);
+		
+		
+		
+//		/* 던전 몬스터 1 텍스트 버튼부착 */
+//		monster1TextBtn = new JButton();
+//		monster1TextBtn.setIcon(monster1Text);
+//		monster1TextBtn.setBounds(89, 543, 247, 103);
+//		monster1TextBtn.setBorderPainted(false);
+//		monster1TextBtn.setContentAreaFilled(false);
+//		monster1TextBtn.setFocusPainted(false);
+//		dungeonPanel.add(monster1TextBtn);
+//
+//		/* 던전 몬스터 2 텍스트 버튼부착 */
+//		monster2TextBtn= new JButton();
+//		monster2TextBtn.setIcon(monster2Text);
+//		monster2TextBtn.setBounds(455, 543, 247, 103);
+//		monster2TextBtn.setBorderPainted(false);
+//		monster2TextBtn.setContentAreaFilled(false);
+//		monster2TextBtn.setFocusPainted(false);
+//		dungeonPanel.add(monster2TextBtn);
+//
+//		/* 던전 몬스터 3 텍스트 버튼부착 */
+//		monster3TextBtn= new JButton();
+//		monster3TextBtn.setIcon(monster3Text);
+//		monster3TextBtn.setBounds(869, 543, 247, 103);
+//		monster3TextBtn.setBorderPainted(false);
+//		monster3TextBtn.setContentAreaFilled(false);
+//		monster3TextBtn.setFocusPainted(false);
+//		dungeonPanel.add(monster3TextBtn);
+//
+//
+//		/* 보스 몬스터 1 텍스트 버튼부착 */
+//		bossMonster1TextBtn = new JButton();
+//		bossMonster1TextBtn.setIcon(bossMonster1Text);
+//		bossMonster1TextBtn.setBounds(1280, 543, 247, 103);
+//		bossMonster1TextBtn.setBorderPainted(false);
+//		bossMonster1TextBtn.setContentAreaFilled(false);
+//		bossMonster1TextBtn.setFocusPainted(false);
+//		dungeonPanel.add(bossMonster1TextBtn);
+
+
+//		/* 던전 몬스터 1 텍스트 부착 */
+//		JLabel monster1Textlb = new JLabel("image/monster1Text.png");
+//		monster1Textlb.setBounds(89, 543, 247, 103);
+//		monster1Textlb.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+//		monster1Textlb.setOpaque(true);
+//		monster1Textlb.setBackground(new Color(192, 192, 192, 150));
+//		
+//		/* 던전 몬스터 2 텍스트 부착 */
+//		JLabel monster2Textlb = new JLabel("image/monster2Text.png");
+//		monster1Textlb.setBounds(455, 543, 247, 103);
+//		monster1Textlb.setBorder(BorderFactory.createEmptyBorder(10 , 10 , 10 , 10));
+//		monster1Textlb.setOpaque(true);
+//		monster1Textlb.setBackground(new Color(192, 192, 192, 150));
+//		
+//		/* 던전 몬스터 3 텍스트 부착 */
+//		JLabel monster3Textlb = new JLabel("image/monster3Text.png");
+//		monster1Textlb.setBounds(869, 543, 547, 103);
+//		monster1Textlb.setBorder(BorderFactory.createEmptyBorder(10 , 10 , 10 , 10));
+//		monster1Textlb.setOpaque(true);
+//		monster1Textlb.setBackground(new Color(192, 192, 192, 150));
+//		
+//		/* 던전 보스 몬스터 1 텍스트 부착 */
+//		JLabel bossMonster1Textlb = new JLabel("image/bossMonster1text.png");
+//		bossMonster1Textlb.setBounds(1348, 543, 164, 103);
+//		bossMonster1Textlb.setBorder(BorderFactory.createEmptyBorder(10 , 10 , 10 , 10));
+//		bossMonster1Textlb.setOpaque(true);
+//		bossMonster1Textlb.setBackground(new Color(192, 192, 192, 150));
+
 	}
 
 	private void add(ImageIcon returnLogo2) {
@@ -128,26 +237,36 @@ public class DungeonPanel extends JPanel {
 
 	public void eventStart() {
 
-		imageMonster1LogoButton.addActionListener(new ActionListener() {
+//		returnLogoBtn.addActionListener(new ActionListener() {
+//
+//			@Override
+//			public void actionPerformed(ActionEvent e) {
+//
+//				returnLogoBtn.(2, 1);
+//
+//			}
+//		});
+		
+		monster1Btn.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
+
 				battlePanel.startBattle(2, 1);
-				
+
 			}
 		});
-		
-		imageMonster2LogoButton.addActionListener(new ActionListener() {
-			
+
+		monster2Btn.addActionListener(new ActionListener() {
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				battlePanel.startBattle(2, 2);
 			}
 		});
-		
-		imageMonster3LogoButton.addActionListener(new ActionListener() {
-			
+
+		monster3Btn.addActionListener(new ActionListener() {
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				battlePanel.startBattle(2, 3);
@@ -155,9 +274,9 @@ public class DungeonPanel extends JPanel {
 				battlePanel.setVisible(true);
 			}
 		});
-		
-		textBossLogoButton.addActionListener(new ActionListener() {
-			
+
+		bossMonster1Btn.addActionListener(new ActionListener() {
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				battlePanel.startBattle(2, 4);
