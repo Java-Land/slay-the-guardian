2e02dffbc06d381e8c8d6306fb3eefa38659d683
yeoun <yeoun@DESKTOP-K8N0GJ7>
2021-08-24 06:56:59 +0900

    20210824 0656 김한솔 개인 브랜치 어드민패널 수정 커밋푸쉬

--- src/com/javaland/stg/panel/AdminPanel.java
+++ src/com/javaland/stg/panel/AdminPanel.java
@@ -40,6 +40,8 @@ public class AdminPanel extends JPanel{
 	private JLabel adminTitlelb;
 	private JScrollPane scrolledTable;
 	private JButton adminExitBtn;
+	private JButton saveBtn;
+	private JLabel resultLb;
 
 	public AdminPanel() {
 		adminPanel = this;
@@ -64,7 +66,7 @@ public class AdminPanel extends JPanel{
 		DefaultTableModel model = new DefaultTableModel(data,title);
 		JTable table = new JTable(model);
 		JScrollPane scrolledTable = new JScrollPane(table);
-		scrolledTable.setBounds(135, 150, 1300, 650);
+		scrolledTable.setBounds(135, 160, 1300, 530);
 		scrolledTable.setBorder(BorderFactory.createLineBorder(Color.gray, 4, true));
 
 		/*테이블 셀 가운데 정렬*/
@@ -75,22 +77,42 @@ public class AdminPanel extends JPanel{
 			columnModel.getColumn(i).setCellRenderer(dtcr);
 		}
 
-		/*테이플 폰트*/
+		/*테이블 폰트*/
 		JTableHeader header = table.getTableHeader();
 		header.setFont(new Font("굴림", Font.BOLD, 15));
  
-		
+		/* 나가기 버튼 */
 		JButton adminExitBtn = new JButton();
 		adminExitBtn.setIcon(adminExit);
 		adminExitBtn.setBounds(1450, 20, 90, 90);
 		adminExitBtn.setBorderPainted(false);
 		adminExitBtn.setContentAreaFilled(false);
 		adminExitBtn.setFocusPainted(false);
+		
+		/* 저장 버튼 */
+		JButton saveBtn = new JButton("저장");
+		saveBtn.setOpaque(true);
+		saveBtn.setBounds(135, 687, 300, 90);
+		saveBtn.setBackground(Color.white);
+		saveBtn.setBorder(BorderFactory.createLineBorder(Color.gray, 4, true));
+		saveBtn.setFont(new Font("굴림", Font.BOLD, 20));
+		
+		/* 저장 결과 출력 라벨 */
+		JLabel resultLb = new JLabel("나가기 전에 꼭 저장하세요!");
+		resultLb.setOpaque(true);
+		resultLb.setBounds(433, 687, 1002, 90);
+		resultLb.setBackground(Color.white);
+		resultLb.setBorder(BorderFactory.createLineBorder(Color.gray, 4, true));
+		resultLb.setVerticalAlignment(SwingConstants.CENTER);
+		resultLb.setHorizontalAlignment(SwingConstants.CENTER);
+		resultLb.setFont(new Font("굴림", Font.BOLD, 20));
 
 		/*AdminPanel에 컴포넌트 추가*/
 		this.add(adminLogolb);
 		this.add(scrolledTable);
 		this.add(adminExitBtn);
+		this.add(saveBtn);
+		this.add(resultLb);
 	}
  
 	public void eventStart() {
@@ -102,6 +124,17 @@ public class AdminPanel extends JPanel{
 //				mainPanel.setVisible(true);
 //			}
 //		});
+		
+//		saveBtn.addActionListener(new ActionListener() {
+//
+//			@Override
+//			public void actionPerformed(ActionEvent e) {
+//				resultLb.setText("저장이 완료 되었습니다.");
+//			}
+//			
+//		});
+		
+		
 	}
 
 	public void panelInit(ScriptPanel scriptPanel, MainPanel mainPanel, StagePanel stagePanel,
